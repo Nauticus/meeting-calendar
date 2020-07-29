376e15d91580acbdd7acd3af3f5b2411fc933940
Emanuil Ganchev <emanuilganchev@Emanuils-MacBook-Pro.local>
2020-07-29 13:25:16 +0300

    Add react-devtools; initial refactor of createStore;

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -1,24 +1,27 @@
 import _ from "lodash/fp";
 import { createStore, Store, combineReducers, PreloadedState } from "redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
 
 import ducks from "./reducers";
-import { Actions, State } from './types';
+import { Actions, State } from "./types";
 
-const getReducersFromDucks = _.pipe(
+const getReducersFromDucks = _.pipe([
     _.values,
-    _.reduce((result, { MOUNT_POINT, reducer }) => Object.assign(result, { [MOUNT_POINT]: reducer }), {})
-);
+    _.reduce((result, { MOUNT_POINT, reducer }) => Object.assign(result, { [MOUNT_POINT]: reducer }), {}),
+]);
 
-const configureStore = (preloadedState?: PreloadedState<State>): Store<State, Actions> => {
-    const reducers = getReducersFromDucks(ducks);
-    const rootReducer = combineReducers(reducers);
+const reducers = getReducersFromDucks(ducks);
 
-    return createStore(
+const rootReducer = combineReducers(reducers);
+
+const configureStore = (preloadedState?: PreloadedState<State>): Store<State, Actions> =>
+    createStore(
         rootReducer,
         preloadedState,
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+        devToolsEnhancer({
+            trace: true,
+            traceLimit: 200,
+        })
     );
-};
 
 export default configureStore;
